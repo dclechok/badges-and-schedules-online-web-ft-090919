c44ea8548aadfc876d7854054f96bab88e2b79fa
Daniel Lechok <dclechok@gmail.com>
2019-09-11 14:48:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,11 @@ def batch_badge_creator(array)
     new_array[c] = "Hello, my name is " + count + "."
     c += 1
   end
+<<<<<<< HEAD
   new_array
+=======
+  printer(new_array)
+>>>>>>> 9080c23b4281810a72f110fc4910d97f4f9e8f1c
   
 end
 
@@ -21,6 +25,7 @@ def assign_rooms(speakers)
     new_array[c] = new_s
     c += 1
   end
+<<<<<<< HEAD
   new_array 
 end
 
@@ -35,3 +40,13 @@ def printer(array)
 		puts item
 	end
 end
+=======
+  printer(new_array)
+end
+
+def printer(array)
+  array.each do |item|
+    puts item
+  end
+end
+>>>>>>> 9080c23b4281810a72f110fc4910d97f4f9e8f1c
